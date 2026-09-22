3d4139279b7fd568aba399eb374005ef8582e793
nhan <nguyenthenhan2006@gmail.com>
2026-09-22 11:01:56 +0700

    lesson 3 create student

--- app.py
+++ app.py
@@ -1,17 +1,48 @@
 from flask import Flask, jsonify, request
+from uuid import uuid4
 
 app = Flask(__name__)
+
+STUDENTS = []
+
+
+# Bài 1
 @app.route("/")
 def index():
     return {"message": "Hello, API!"}
-# GET /health
+
+
+# Bài 2
 @app.route("/health", methods=["GET"])
 def health():
     return jsonify({"status": "ok"}), 200
-# POST /echo
+
 @app.route("/echo", methods=["POST"])
 def echo():
     data = request.get_json(silent=True) or {}
     return jsonify({"you_sent": data}), 200
+
+# Bài 3
+@app.route("/students", methods=["POST"])
+def create_student():
+    body = request.get_json(silent=True) or {}
+
+    name = body.get("name")
+
+    # Kiểm tra name
+    if not name:
+        return jsonify({"error": "name là bắt buộc"}), 400
+
+    student = {
+        "id": str(uuid4()),
+        "name": name,
+        "gpa": body.get("gpa", 0.0)
+    }
+
+    STUDENTS.append(student)
+
+    return jsonify(student), 201
+
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
