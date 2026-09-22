d1752793ba9804892dfd2fa7305c770b61181838
nhan <nguyenthenhan2006@gmail.com>
2026-09-22 10:33:18 +0700

    Session 1: Hello API

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# app.py — Bài 1: Hello API
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,4 +11,8 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(
+        host="127.0.0.1",
+        port=5000,
+        debug=True
+    )
