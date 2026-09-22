3b83f4fef54a58bffb16f37396151202df44f6c4
nhan <nguyenthenhan2006@gmail.com>
2026-09-22 17:37:36 +0700

    lesson 2

--- week2/app.py
+++ week2/app.py
@@ -53,5 +53,67 @@ def get_book(book_id):
     return jsonify(book)
 
 
+# PUT /books/<id>
+@app.put("/books/<int:book_id>")
+def update_book(book_id):
+    book = next(
+        (book for book in BOOKS if book["id"] == book_id),
+        None
+    )
+
+    if book is None:
+        return jsonify({"error": "not found"}), 404
+
+    data = request.get_json()
+
+    if not data.get("title") or not data.get("author"):
+        return jsonify({
+            "error": "title and author required"
+        }), 422
+
+    book["title"] = data["title"]
+    book["author"] = data["author"]
+
+    return jsonify(book), 200
+
+
+# PATCH /books/<id>
+@app.patch("/books/<int:book_id>")
+def patch_book(book_id):
+    book = next(
+        (book for book in BOOKS if book["id"] == book_id),
+        None
+    )
+
+    if book is None:
+        return jsonify({"error": "not found"}), 404
+
+    data = request.get_json()
+
+    if "title" in data:
+        book["title"] = data["title"]
+
+    if "author" in data:
+        book["author"] = data["author"]
+
+    return jsonify(book), 200
+
+
+# DELETE /books/<id>
+@app.delete("/books/<int:book_id>")
+def delete_book(book_id):
+    book = next(
+        (book for book in BOOKS if book["id"] == book_id),
+        None
+    )
+
+    if book is None:
+        return jsonify({"error": "not found"}), 404
+
+    BOOKS.remove(book)
+
+    return "", 204
+
+
 if __name__ == "__main__":
     app.run(debug=True)
